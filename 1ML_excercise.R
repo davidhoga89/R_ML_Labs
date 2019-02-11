9f03d5eb69ff4de532abca5978bcf68c8dcd9298
David <david@Davidhogas-MacBook-Pro.local>
2019-02-10 21:02:19 -0500

    1st exercise completed

--- 1ML_excercise.R
+++ 1ML_excercise.R
@@ -1,9 +1,9 @@
 library(dslabs)
 library(dplyr)
 library(lubridate)
+library(caret)
 
 data("reported_heights")
-
 dat <- mutate(reported_heights, date_time = ymd_hms(time_stamp)) %>%
   filter(date_time >= make_date(2016, 01, 25) & date_time < make_date(2016, 02, 1)) %>%
   mutate(type = ifelse(day(date_time) == 25 & hour(date_time) == 8 & between(minute(date_time), 15, 30), "inclass","online")) %>%
@@ -11,4 +11,26 @@ dat <- mutate(reported_heights, date_time = ymd_hms(time_stamp)) %>%
 
 y <- factor(dat$sex, c("Female", "Male"))
 x <- dat$type
-dat
+#proportion of females in class and online
+table(dat)
+dat %>% group_by(type) %>% summarize(prop_female = mean(sex == "Female"))
+
+
+set.seed(2)
+test_index <- createDataPartition(y,times=1,p=0.25, list=FALSE)
+train_set <- dat[-test_index, ]
+test_set <- dat[test_index, ]
+
+#accuracy to predict sex using type
+y_hat <- ifelse(x == "inclass", "Female", "Male")
+#y_hat
+mean(y == y_hat)
+
+#Confussion Matrix
+table(predicted=y_hat, actual=y)
+confusionMatrix(data = y_hat, reference = y)
+
+#TP/(TP + FN) sensitivity
+26/(26+42)
+#TN/(TN + FP) specificity
+69/(69+13)

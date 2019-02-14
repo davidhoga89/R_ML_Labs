f4cdfd16f7c5b56c23a9437df5e80aabad79ffd3
David <david@Davidhogas-MacBook-Pro.local>
2019-02-13 23:33:04 -0500

    1st exercise completed

--- 2ML_exercise.R
+++ 2ML_exercise.R
@@ -51,7 +51,47 @@ mean(y_hat==test$Species)
 #answer from professor
 predictions2 <- foo(train[,3])
 rangedValues2 <- seq(range(train[,3])[1],range(train[,3])[2],by=0.1)
-cutoffs <-rangedValues2[which(predictions2==max(predictions2))]
+cutoff2 <-rangedValues2[which(predictions2==max(predictions2))]
+cutoff2
+#accuracy using Petal.Length as feature
+y_hat2 <- ifelse(test[,3]>cutoff2[1],'virginica','versicolor')
+mean(y_hat2==test$Species)
 
-y_hat <- ifelse(test[,3]>cutoffs[1],'virginica','versicolor')
+predictions3 <- foo(train[,4])
+rangedValues3 <- seq(range(train[,4])[1],range(train[,4])[2],by=0.1)
+cutoff3 <-rangedValues3[which(predictions3==max(predictions3))]
+cutoff3
+#accuracy using Petal.Width as feature
+y_hat3 <- ifelse(test[,4]>cutoff3[2],'virginica','versicolor')
+mean(y_hat3==test$Species)
+#accuracy using Petal.Width and Petal.Length as features
+y_hat_final <- ifelse((test[,4]>cutoff3[2]) | (test[,3]>cutoff2[1]),'virginica','versicolor')
+mean(y_hat_final==test$Species)
+
+#----------------------------------------------------------------------
+#alternative answer from professor
+library(caret)
+data(iris)
+iris <- iris[-which(iris$Species=='setosa'),]
+y <- iris$Species
+
+plot(iris,pch=21,bg=iris$Species)
+
+set.seed(2)
+test_index <- createDataPartition(y,times=1,p=0.5,list=FALSE)
+test <- iris[test_index,]
+train <- iris[-test_index,]
+
+petalLengthRange <- seq(range(train[,3])[1],range(train[,3])[2],by=0.1)
+petalWidthRange <- seq(range(train[,4])[1],range(train[,4])[2],by=0.1)
+cutoffs <- expand.grid(petalLengthRange,petalWidthRange)
+
+id <- sapply(seq(nrow(cutoffs)),function(i){
+  y_hat <- ifelse(train[,3]>cutoffs[i,1] | train[,4]>cutoffs[i,2],'virginica','versicolor')
+  mean(y_hat==train$Species)
+}) %>% which.max
+
+optimalCutoff <- cutoffs[id,] %>% as.numeric
+optimalCutoff
+y_hat <- ifelse(test[,3]>optimalCutoff[1] & test[,4]>optimalCutoff[2],'virginica','versicolor')
 mean(y_hat==test$Species)
